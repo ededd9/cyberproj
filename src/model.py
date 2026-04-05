7830fd51ee9f31e7582ae951a929cc9d28026dd1
Edward Hurtado <edhur9@gmail.com>
2026-04-05 17:40:24 -0400

    cleaned up model file, ended up using logistic regression

--- src/model.py
+++ src/model.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import re
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -7,15 +6,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
 from sklearn.metrics import recall_score, precision_score, f1_score
-
-#==========================#
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 
-
-
-#==========================#
 #function that removes links, numbers, etc from message/text
 def clean_text(message):
     cleanedmessage = message.lower()
@@ -24,15 +18,15 @@ def clean_text(message):
     cleanedmessage = re.sub(r'[^a-z\s]', '',cleanedmessage)
     cleanedmessage = re.sub(r'\s+',' ', cleanedmessage)
     return cleanedmessage
-df = pd.read_csv("./data/spam.csv", encoding="latin-1")
 #keep only v1 and v2 columns as thats all the important data in
+df = pd.read_csv("./data/spam.csv", encoding="latin-1")
 df = df[["v1","v2"]]
 df.columns = ["label", "text"]
 df["label"] = df["label"].map({"ham":0, "spam": 1})
 df.drop_duplicates(inplace=True)
 df.dropna(inplace=True)
 df["clean_text"] = df["text"].apply(clean_text)
-
+#convvert text to numbers
 vectorizer = TfidfVectorizer(
     stop_words = "english",
     max_features = 5000,
@@ -40,12 +34,9 @@ vectorizer = TfidfVectorizer(
     min_df = 2,
     sublinear_tf=True
 )
-
 X = vectorizer.fit_transform(df["clean_text"])
 y = df["label"]
-
-print(X.shape)
-
+#train / test split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y,
     test_size = 0.2,
@@ -55,6 +46,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 print(f"Training samples: {X_train.shape[0]}")
 print(f"Testing samples:  {X_test.shape[0]}")
+#train and compare models 
 models = {
     "Naive Bayes": MultinomialNB(alpha=0.1),
     "Logistic Regression": LogisticRegression(C=1.0, max_iter = 1000,class_weight="balanced"),
@@ -69,7 +61,7 @@ for name, model in models.items():
     precision = precision_score(y_test,preds)
     f1 = f1_score(y_test, preds)
     print(f"{name:25} recall: {recall:.3f} precision {precision:.3f} f1: {f1:.3f}")
-
+#threshold tuning on logistic regression
 lr_model = models["Logistic Regression"]
 probs = lr_model.predict_proba(X_test)[:,1]
 print("\nLogistic Regression threshold tuning:")
@@ -79,17 +71,25 @@ for threshold in [0.5,0.4,0.3,0.2,0.1]:
     precision = precision_score(y_test, preds)
     f1 = f1_score(y_test, preds)
     print(f"Threshold {threshold} → recall: {recall:.3f}  precision: {precision:.3f}  f1: {f1:.3f}")
-# model = MultinomialNB(alpha=0.1)
-# model.fit(X_train, y_train)
-# print("Model trained")
 
-# predictions = model.predict(X_test)
-# print(classification_report(y_test,predictions,target_names=["Safe","Threat"]))
-
-#test different thresholds
-# probs = model.predict_proba(X_test)[:, 1]
-# for threshold in [0.5,0.4,0.3,0.2]:
-#     preds = (probs >= threshold).astype(int)
-#     recall = recall_score(y_test, preds)
-#     precision = precision_score(y_test,preds)
-#     print(f"Threshold {threshold} -> recall: {recall:.3f} precision {precision:.3f}")
+#using model Logistic Regression and saving it to disk
+final_model = models["Logistic Regression"]
+with open("model.pkl", "wb") as f:
+    pickle.dump(final_model,f)
+with open("vectorizer.pkl", "wb") as f:
+    pickle.dump(vectorizer,f)
+print("Model and vectorizer saved.")
+#load pre-trained model and the pre fitted vectorizer from disk : model has all its
+#learned weights and vectorizer already knows the vocab / IDF scores from training
+loaded_model = pickle.load(open("model.pkl", "rb"))
+loaded_vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+test_messages = ["Congratulations you won a free prize click here now","Please respond to the customers email ASAP","FREE BITCOIN IN 24 HOURS","Pizza party in the third floor tomorrow at 2:00 PM"]
+cleaned= []
+#list comprehension instead of for loop
+cleaned = [clean_text(message) for message in test_messages]
+vec = loaded_vectorizer.transform(cleaned)
+probs = loaded_model.predict_proba(vec)[:,1]
+label = []
+for message, prob in zip(test_messages,probs):
+    label = "THREAT" if prob >= 0.3 else "SAFE"
+    print(f"\nTest prediction [{label}] - {prob:.3f} confidence for message -> {message}")
